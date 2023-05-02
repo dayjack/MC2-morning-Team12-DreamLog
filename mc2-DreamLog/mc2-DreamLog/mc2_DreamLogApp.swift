c80a79300c380b7dfb3159cd085eef3247f2cc35
Taerogrammer <hyung54321@naver.com>
2023-05-02 23:48:57 +0900

    파일 생성

---
+++ mc2-DreamLog/mc2-DreamLog/mc2_DreamLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  mc2_DreamLogApp.swift
+//  mc2-DreamLog
+//
+//  Created by KimTaeHyung on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct mc2_DreamLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
