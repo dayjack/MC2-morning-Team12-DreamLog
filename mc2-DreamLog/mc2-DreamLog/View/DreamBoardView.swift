86567bef801a7da3e4ef56aa2988abaead2f5763
Taerogrammer <hyung54321@naver.com>
2023-05-04 17:12:28 +0900

    [Feat] DreamLog#21 - 응원 한마디 저장 완료

--- mc2-DreamLog/mc2-DreamLog/View/DreamBoardView.swift
+++ mc2-DreamLog/mc2-DreamLog/View/DreamBoardView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct DreamBoardView: View {
     
     @State var text = ""
+    @State private var showingAlert: Bool = false
+    @State private var cheerText: String = ""
+    @State private var confirmAlert: Bool = false
     
     var body: some View {
         BgColorGeoView { geo in
@@ -49,11 +52,28 @@ struct DreamBoardView: View {
                         Text("나에게 주는 응원 한마디")
                         Spacer()
                         Button {
-                            
+                            showingAlert = true
                         } label: {
                             Image(systemName: "plus")
                                 .foregroundColor(.textGreen)
                         }
+                        .alert("나에게 주는 응원 한마디를\n작성해주세요", isPresented: $showingAlert, actions: {
+                            TextField("응원의 한 마디를 작성해보아요", text: $cheerText)
+                            
+                            Button("완료", action: {
+                                confirmAlert = true
+                            })
+                            Button("취소", role: .cancel, action: {})
+                        })
+                        .alert(isPresented: $confirmAlert, content: {
+                            Alert(title: Text("\(cheerText)으로\n응원을 추가하시겠어요?"),
+                                  message: Text("작성하신 응원은 위젯에 표시됩니다."),
+                                  primaryButton: .default(Text("확인"), action: {
+                                      print("\(cheerText) saved")
+                                  }),
+                                  secondaryButton: .cancel(Text("취소"), action: {
+                                  }))
+                        })
                     }
                 }
                 .padding(.horizontal, 16)
