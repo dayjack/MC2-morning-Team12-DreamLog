0990fb62e9284ac6ff3c68a695f46d70d9ed73c5
Moonkey <moonkey48@icloud.com>
2023-05-11 15:42:10 +0900

    [Feat] DreamLog#91 - DreamLogView 레이아웃 구성

--- mc2-DreamLog/mc2-DreamLog/View/DreamLogView.swift
+++ mc2-DreamLog/mc2-DreamLog/View/DreamLogView.swift
@@ -8,18 +8,38 @@
 import SwiftUI
 
 struct DreamLogView: View {
+    @State var tab1Model = Tab1Model()
     @State var boardList = [
-        "BoardDummy"
+        UIImage(named: "BoardDummy")
     ]
     
     var body: some View {
+    
         ScrollView {
             VStack(spacing: 0) {
                 ForEach(boardList, id: \.self) { boardImage in
                     ZStack {
-                        Image(uiImage: UIImage(named: boardImage)!)                                                         
+                        Image(uiImage: boardImage!)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 320)
+                        
+                        VStack(spacing: 0) {
+                            Spacer()
+                            Text("2023.04.01 ~ 2023.04.25")
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity)
+                                .background(Color.white)
+                                .font(Font.system(size: 20, weight: Font.Weight.bold))
+                                .foregroundColor(Color.gray)
+                        }
+                        Spacer()
+                            .frame(height: 20)
                     }
                     .cornerRadius(20)
+                    .shadow(color: Color.shadowGray, radius: 4)
+                    .padding()
+                    
                 }
                 
             }
