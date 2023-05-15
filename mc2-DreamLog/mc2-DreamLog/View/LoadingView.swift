e861a37cf9ac8ef30ae180c7543e65342d86eb5b
Moonkey <moonkey48@icloud.com>
2023-05-15 10:47:46 +0900

    [Fix] DreamLog#151 - Process indicator 디자인 수정(LoadingView)

--- mc2-DreamLog/mc2-DreamLog/View/LoadingView.swift
+++ mc2-DreamLog/mc2-DreamLog/View/LoadingView.swift
@@ -11,7 +11,7 @@ struct LoadingView<Content>: View where Content: View {
     
     @Binding var isShowing: Bool  // should the modal be visible?
     var content: () -> Content
-    var text: String? = "Wait..." // the text to display under the ProgressView - defaults to "Loading..."
+    var text: String? = "보드 저장중" // the text to display under the ProgressView - defaults to "Loading..."
     
     var body: some View {
         GeometryReader { geometry in
@@ -19,25 +19,27 @@ struct LoadingView<Content>: View where Content: View {
                 // the content to display - if the modal is showing, we'll blur it
                 content()
                     .disabled(isShowing)
-                    .blur(radius: isShowing ? 2 : 0)
+                    .blur(radius: isShowing ? 3 : 0)
                 
                 // all contents inside here will only be shown when isShowing is true
                 if isShowing {
                     // this Rectangle is a semi-transparent black overlay
                     Rectangle()
-                        .fill(Color.black).opacity(isShowing ? 0.6 : 0)
+                        .fill(Color.black).opacity(isShowing ? 0.5 : 0)
                         .edgesIgnoringSafeArea(.all)
                     
                     // the magic bit - our ProgressView just displays an activity
                     // indicator, with some text underneath showing what we are doing
-                    VStack(spacing: 48) {
-                        ProgressView().scaleEffect(2.0, anchor: .center)
-                        Text(text ?? "Loading...").font(.title).fontWeight(.semibold)
+                    VStack(spacing: 15) {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                            .scaleEffect(1.5, anchor: .center)
+                            .foregroundColor(Color.white)
+                        Text(text ?? "보드 저장중")
+                            .font(.system(size: 18))
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
                     }
-                    .frame(width: 250, height: 200)
-                    .background(Color.white)
-                    .foregroundColor(Color.primary)
-                    .cornerRadius(16)
                 }
             }
         }
